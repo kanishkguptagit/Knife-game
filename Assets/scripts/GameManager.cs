2dc72ff5454bdc22aaecd8f51f2c6eae1f1c9678
kanishkguptagit <1905902@kiit.ac.in>
2020-06-22 19:32:42 +0530

    Initial Commit

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public GameObject knife;
+    public Vector2 pos;
+    public Button restart;
+    public SpriteRenderer[] sr = new SpriteRenderer[8];
+    private int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        knife.SetActive(true);
+        pos = knife.transform.position;
+        CreatingKnife();
+        index = 0;
+    }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void CreatingKnife()
+    {
+        Instantiate(knife, pos, Quaternion.identity);        
+    }
+
+    public void onPressRestart()
+    {
+        knife.SetActive(true);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void activateRestart()
+    {
+        knife.SetActive(false);
+        restart.gameObject.SetActive(true);
+    }
+
+    public void knifeLeft()
+    {
+        sr[index].color = new Color32(255, 255, 255, 70);
+        index++;
+        if(index == 8)
+        {
+            activateRestart();
+        }
+    }
+}
---
+++ Assets/scripts/knife_shooter.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class knife_shooter : MonoBehaviour
+{
+    
+    private Rigidbody2D rb;
+    private BoxCollider2D bx;
+    private ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bx = GetComponent<BoxCollider2D>();
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(new Vector2(0, 40), ForceMode2D.Impulse);
+            rb.gravityScale = 1;
+        }
+              
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "log")
+        {
+            rb.velocity = new Vector2(0, 0);
+            rb.bodyType = RigidbodyType2D.Kinematic;
+            this.transform.SetParent(collision.collider.transform);
+            bx.offset = new Vector2(bx.offset.x, -1.4f);
+            bx.size = new Vector2(bx.size.x, 3.3f);
+            ps.Play();
+
+            GameManager.Instance.knifeLeft();
+            GameManager.Instance.CreatingKnife();
+        }        
+
+        if (collision.collider.tag == "knife")
+        {            
+            GameManager.Instance.activateRestart();
+        }
+        
+
+    }
+
+}
